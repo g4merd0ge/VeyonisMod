9d3072523f0a95e203bb6a5723a48caf83162c80
g4merd0ge <a.e.sintsov@gmail.com>
2025-07-21 14:09:54 +0300

    Initial Commit Failed

---
+++ src/main/java/com/dodiks/veyonis/VeyonisModClient.java
@@ -0,0 +1,11 @@
+package com.dodiks.veyonis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeyonisModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/dodiks/veyonis/VeyonisModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.dodiks.veyonis;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VeyonisModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
